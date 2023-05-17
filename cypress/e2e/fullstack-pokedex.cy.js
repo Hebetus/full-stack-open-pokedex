928961f850760294b8c3f00bec172c073c86af61
Emil Hellberg <ephell@utu.fi>
2023-05-17 15:00:03 +0300

    11.9

--- cypress/e2e/fullstack-pokedex.cy.js
+++ cypress/e2e/fullstack-pokedex.cy.js
@@ -4,4 +4,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('can navigate to chosen pokemon', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('venusaur').click()
+    cy.contains('chlorophyll')
+  })
 })
