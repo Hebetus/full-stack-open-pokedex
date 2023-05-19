d1df83dbd63ed59e5ca6caf1ac2d6acf25d976b8
Emil Hellberg <ephell@utu.fi>
2023-05-19 18:43:18 +0300

    11.2

--- health_check.sh
+++ health_check.sh
@@ -2,4 +2,14 @@
 
 echo "Hello from shell script"
 
-exit 0
+VAR_A=$(curl http://localhost:5000/health)
+
+echo $VAR_A
+
+if [ "$VAR_A" == "ok" ]; then
+    echo "healthy"
+    exit 0
+else
+    echo "not healthy"
+    exit 1
+fi
